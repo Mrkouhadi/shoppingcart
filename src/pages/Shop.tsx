e841dca842bc05a17fb531ff7a003c5457b9e192
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-06-26 13:29:42 +0800

    imported shopitems

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap';
+import ShopItem from '../components/ShopItem';
+import ShopItems from '../data/items.json';
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <h1>STORE</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {ShopItems.map(item =>(
+         <Col key={item.id}> <ShopItem {...item} /> </Col>
+        ))}
+      </Row>
+    </div>
   )
 }
 
