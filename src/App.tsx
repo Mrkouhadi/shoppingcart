c65eec74177518217262d388a22c89e642572b29
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-06-27 23:38:03 +0800

    added context provider of the cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap"
 import {Routes, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/CartContext"
 import About from "./pages/About"
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
  
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar/>
       <Container>
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/about" element={<About/>}/>
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   )
 }
 export default App
