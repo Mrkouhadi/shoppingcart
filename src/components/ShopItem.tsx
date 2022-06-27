c735c7bdbf25031acf5c7c7e903071c187dbd070
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-06-27 22:14:44 +0800

    added plus and minus buttons

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap";
+import formatCurrency from "../utils/formatCurrency";
 
 type shopItemProps = {
     name:string,
@@ -7,12 +8,32 @@ type shopItemProps = {
     imgUrl:string
 }
 
-
-
 const ShopItem = ({id, name, imgUrl, price}:shopItemProps) => {
+    const quantity = 10;
+
   return (
-    <Card>
+    <Card className="h-100">
         <Card.Img src={imgUrl} variant="top" height="200px" style={{objectFit:'cover'}} />
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                <span className="fs-4">{name}</span>
+                <span className="text-muted ms-2">{formatCurrency(price)}</span>
+            </Card.Title>
+            <div className="mt-auto">
+                {
+                    quantity === 0 ? (
+                        <Button className="bg-success w-100" >Add to Cart</Button>
+                    ): <div className="d-flex align-items-center flex-column" style={{gap:".5rem"}}>
+                          <div className="d-flex align-items-center justify-content-center " style={{gap:".5rem"}}>
+                            <Button>-</Button>
+                            <div><span className="fs-3">{quantity}</span> in Cart</div>
+                            <Button>+</Button>
+                          </div>
+                          <Button variant="danger">Remove</Button>
+                       </div>
+                }
+            </div>
+        </Card.Body>
     </Card>
   )
 }
