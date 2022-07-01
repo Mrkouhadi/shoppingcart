1547495bc4938a77a1de07e5ac6ea1b7ba2d7598
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-07-02 00:38:46 +0800

    added Stack

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -16,8 +16,8 @@ const Cart = ({isOpen}:ShoppingCartProps) => {
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        {/* <Stack gap={3}>
-          {cartItems.map(item => (
+        <Stack gap={3}>
+          {/* {cartItems.map(item => (
             <CartItem key={item.id} {...item} />
           ))}
           <div className="ms-auto fw-bold fs-5">
@@ -28,8 +28,8 @@ const Cart = ({isOpen}:ShoppingCartProps) => {
                 return total + (item?.price || 0) * cartItem.quantity
               }, 0)
             )}
-          </div>
-        </Stack> */}
+          </div> */}
+        </Stack> 
       </Offcanvas.Body>
     </Offcanvas>
   )
