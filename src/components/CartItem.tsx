7b42a75b81cf47b52012ab0f8f99ede4924fad9c
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-07-03 23:34:14 +0800

    added content to cartitem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -11,8 +11,13 @@ const CartItem = ({id, quantity}:cartItemProps) => {
     if(item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
         <img src={item.imgUrl} style={{width:'130px', height:'75', objectFit:'cover'}}/>
+        <div className="me-auto">
+            <div>
+                {item.name} {quantity > 1 && <span className="text-muted" style={{fontSize:'.7rem'}}>{quantity}X</span>}
+            </div>
+        </div>
     </Stack>
   )
 }
